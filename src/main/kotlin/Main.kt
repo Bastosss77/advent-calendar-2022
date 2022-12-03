e66bba9a5f818cddec50d9227340d531c76b2069
Bastien Le Bret <blb@citygo.me>
2022-12-03 15:18:41 +0100

    Day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 import puzzles.day1
 import puzzles.day2
+import puzzles.day3
 
 fun main(args: Array<String>) {
     puzzle { day1() }
     puzzle { day2() }
+    puzzle { day3() }
 }
 
 private fun puzzle(puzzle: () -> Unit) {
